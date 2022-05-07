61016499f3db4b0255fcf5f4dcdf2fe771c83e87
eneller <erikneller@gmx.de>
2022-05-07 21:11:34 +0200

    add dotfiles

--- i3setup.sh
+++ i3setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 install_pulse # run the manjaro pulseAudio install script
+git clone --bare git@github.com:eneller/.dotfiles.git $HOME/.dotfiles
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles checkout
