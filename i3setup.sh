4b75b9a35159c1b9fae5f13f9a27d3792272eb7a
eneller <erikneller@gmx.de>
2022-08-04 08:32:40 +0200

    add basic stuff

--- i3setup.sh
+++ i3setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
+GITMIRROR=git@github.com:eneller
 #----------------------------------------#
-# This is a setup script for my custom manjaro install, based on manjaro-xfce with i3 as my main window manager.
-
-
 # INSTALL i3 
 
 
 
 # SET UP DOTFILES
-git clone --bare git@github.com:eneller/.dotfiles.git $HOME/.dotfiles
+git clone --bare "$GITMIRROR/.dotfiles.git" $HOME/.dotfiles
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 #TODO find a way to move files to file.old (ansible?)
 dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
 
+# Set up some basic stuff
+cd ~ && mkdir Code Arbeit; 
+cd ~/Code && git clone "$GITMIRROR/.dotfiles" dotfiles; \
+  git clone "$GITMIRROR/ansible"
 
 # Stuff from the initial clean i3 install attempt
 #install_pulse # run the manjaro pulseAudio install script
