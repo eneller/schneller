307f705b61f1bab54284b38654caea4e8117ab75
eneller <erik.neller@gmx.de>
2022-08-08 13:16:37 +0200

    notes

--- i3.sh
+++ i3.sh
@@ -5,3 +5,4 @@
 #install_pulse # run the manjaro pulseAudio install script
 #sudo pacman -S network-manager-applet # install network manager + applet here
 
+# remove manjaro-i3-settings and install i3-wm (will remove i3-gaps which is a dependency of the settings)
