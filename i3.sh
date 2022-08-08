d3df403905aa70da3e2139b3fad9baa95980da93
eneller <erik.neller@gmx.de>
2022-08-08 14:40:11 +0200

    stuff

--- i3.sh
+++ i3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# manjaro i3 settings can be found at https://gitlab.manjaro.org/profiles-and-settings/desktop-settings/-/tree/master/community/i3
 # Execute basic setup first
 ./basic.sh
 # Stuff from the initial clean i3 install attempt
@@ -6,3 +7,4 @@
 #sudo pacman -S network-manager-applet # install network manager + applet here
 
 # remove manjaro-i3-settings and install i3-wm (will remove i3-gaps which is a dependency of the settings)
+# remove palemoon-bin, mousepad
